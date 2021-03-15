8fb790aa1ecdeb7abebbbe204fb14388c8c6b117
Greg Fisher <gnfisher@thoughtbot.com>
2021-03-15 09:59:16 -0400

    User aliasrc file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,10 @@
 set TERM "xterm-256color"
 set EDITOR nvim
+set JAVA_HOME "/usr/bin/java"
 
 set PATH $PATH "$PATH:/home/greg/.bin"
 set PATH /home/greg/.asdf/installs/nodejs/15.11.0/.npm/bin $PATH
 set PATH /home/greg/.local/share/coursier/bin $PATH
-
-set JAVA_HOME "/usr/bin/java"
 
 fish_vi_key_bindings
 set pure_enable_git true
@@ -13,5 +12,4 @@ set pure_enable_single_line_prompt false
 
 source ~/.asdf/asdf.fish
 
-alias vim nvim
-
+source ~/.aliasrc
