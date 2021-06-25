c57274051297333efcd433f45d0a21adb80b9ded
Greg Fisher <gnfisher@thoughtbot.com>
2021-06-25 10:44:55 -0400

    Prompt love

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -21,10 +21,7 @@ function mcd
 end
 
 function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
     printf '%s%s%s@%s%s %s%s%s$ ' \
-    (set_color green) $USER (set_color white) (set_color cyan) (hostname -s) \
-    (set_color brwhite) (prompt_pwd) (set_color normal)
+    (set_color -o green) $USER (set_color white) (set_color cyan) (hostname -s) \
+    (set_color brwhite) (basename $PWD) (set_color normal)
 end
