cce60eea8a442151683fb9e89229ec53a027c2c6
Greg Fisher <greg@gnfisher.com>
2022-08-05 19:34:48 -0400

    Test config, tmux fix

--- config/nvim/lua/gnfisher/test/setup.lua
+++ config/nvim/lua/gnfisher/test/setup.lua
@@ -5,7 +5,7 @@ end
 test.setup({
 termOpts = {
   direction = 'horizontal',   -- terminal's direction ("horizontal"|"vertical"|"float")
-  go_back = true,          -- return focus to original window after executing
+  go_back = fals,          -- return focus to original window after executing
   stopinsert = 'auto',      -- exit from insert mode (true|false|"auto")
   keep_one = true,          -- keep only one terminal for testing
   },
