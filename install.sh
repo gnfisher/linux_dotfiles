bcad65b75b3200cb50c4fdbfe793d4afa7f70c46
Greg Fisher <greg@gnfisher.com>
2022-08-17 08:46:02 -0400

    Installing neovim in codespaces

--- install.sh
+++ install.sh
@@ -4,15 +4,33 @@ exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
 
-echo 'Installing dotfiles, maybe'
+function fancy_echo() {
+        local fmt="$1"; shift
+
+        printf "\\n$fmt\\n" "$@"
+}
 
 if [[ "$CODESPACES" = "true" ]]; then
+  fancy_echo "Installing apt packages..." 
   sudo apt-get install -y rcm tmux universal-ctags ripgrep bat jq fzf fish
+
+  fancy_echo "Installing neovim..."
+  curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+  chmod u+x nvim.appimage
+  ./nvim.appimage --appimage-extract
+  ./squashfs-root/AppRun --version
+  sudo mv squashfs-root / && ln -s /squashfs-root/AppRun $HOME/.local/bin/nvim
+
+  fancy_echo "Installing dotfiles..."
   rcup -f -v -d .
+
+  fancy_echo "Setting up neovim..."
   nvim +PluginInstall +qa
 
   # Default to HTTPS for GitHub access
   git config --global url.https://github.com/.insteadOf git@github.com:
 
   sudo chsh -s $(which fish) $(whoami)
+
+  fancy_echo "Done."
 fi
