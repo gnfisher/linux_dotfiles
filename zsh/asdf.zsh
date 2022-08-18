35c5edca7b81631d65dbe4a6c7823b4976969f13
Greg Fisher <greg@gnfisher.com>
2022-08-17 21:15:07 -0400

    zsh: full path to brew, dunno

--- zsh/asdf.zsh
+++ zsh/asdf.zsh
@@ -1,5 +1,5 @@
 if [[ `uname` == "Darwin" ]]; then
-  source "$(brew --prefix asdf)/libexec/asdf.sh"
+  source "$(/opt/homebrew/bin/brew --prefix asdf)/libexec/asdf.sh"
 else
   source $HOME/.asdf/asdf.sh
 fi
