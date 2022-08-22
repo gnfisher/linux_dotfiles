2119bc52f6e39da07827b5031a938f0e72217e07
Greg Fisher <greg@gnfisher.com>
2022-08-22 08:59:59 -0400

    install: codespaces try 9999

--- install.sh
+++ install.sh
@@ -5,22 +5,23 @@ exec 2>&1
 set -x
 
 function fancy_echo() {
-        local fmt="$1"; shift
+  local fmt="$1"; shift
 
-        printf "\\n$fmt\\n" "$@"
+  printf "\\n$fmt\\n" "$@"
 }
 
 if [[ "$CODESPACES" = "true" ]]; then
   fancy_echo "Installing apt packages..." 
   sudo apt-get install -y rcm tmux universal-ctags ripgrep bat jq fzf
 
-  fancy_echo "Installing neovim..."
-  curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-  chmod u+x nvim.appimage
-  ./nvim.appimage --appimage-extract
-  ./squashfs-root/AppRun --version
-  sudo mv squashfs-root / && ln -s /squashfs-root/AppRun $HOME/.local/bin/nvim
-
+  if ! command -v nvim &> /dev/null; then
+    fancy_echo "Installing neovim..."
+    curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+    chmod u+x nvim.appimage
+    ./nvim.appimage --appimage-extract
+    ./squashfs-root/AppRun --version
+    sudo mv squashfs-root / && ln -s /squashfs-root/AppRun $HOME/.local/bin/nvim
+  fi
 
   fancy_echo "Installing dotfiles..."
   mv $HOME/.zshrc $HOME/.zshrc.old
