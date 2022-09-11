41f387e89957dfef81f35a468c546b04a28ba76f
Greg Fisher <greg@gnfisher.com>
2022-09-11 13:07:35 -0400

    Fix brew path in zsh

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -1,6 +1,6 @@
 # Load completion functions
 if [[ `uname` == "Darwin" ]]; then
-  fpath=$(brew --prefix)/share/zsh-completions:$fpath
+  fpath=(/opt/homebrew/share/zsh-completions:$fpath)
 else
   fpath=(/usr/local/share/zsh/site-functions $fpath)
 fi
