80be0a9335dce4bcd089f9ae343a2992be192d37
Greg Fisher <greg@gnfisher.com>
2022-10-28 16:24:54 -0400

    Fix that path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
-if [[ `uname` == "Darwin" ]]; then
+if [[ "$CODESPACES" = "true" ]]; then
   export PATH=/workspaces/github/vendor/node:/workspaces/github/bin:/workspaces/github/node_modules/.bin:$PATH
 fi
