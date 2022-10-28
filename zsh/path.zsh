5a1a320161ea29e7bd0e67e04e56a95aebbc4ffd
Greg Fisher <greg@gnfisher.com>
2022-10-28 16:32:40 -0400

    Path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,3 @@
 if [[ "$CODESPACES" = "true" ]]; then
-  export PATH=/workspaces/github/vendor/node:/workspaces/github/bin:/workspaces/github/node_modules/.bin:$PATH
+  export PATH=/workspaces/github/bin:/workspaces/github/node_modules/.bin:$PATH
 fi
